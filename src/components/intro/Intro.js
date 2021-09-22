b7b2ff5ce1968c2057fac734ad67b52f693c7e85
Asen Sanjula <asensanjula@gmail.com>
2021-09-22 21:02:26 +0530

    feat: add ityped for span text

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import './intro.scss'
+import React, {useEffect, useRef} from 'react';
+import './intro.scss';
+import { init } from 'ityped'
+
 
 function Intro(props) {
+
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current,
+            {
+                showCursor: true,
+                backDelay:1000,
+                backSpeed:  60,
+                strings: ['React JS', 'Bootstrap', 'Redux', 'Mongo DB' ]
+            })
+    },[])
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -13,7 +26,7 @@ function Intro(props) {
                 <div className="wrapper">
                     <h2>Hello There, I'm</h2>
                     <h1>Asen Sanjula</h1>
-                    <h3>Front-End Developer | React JS</h3>
+                    <h3>Front-End Developer | <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src={process.env.PUBLIC_URL + "/assets/down.png"} alt=""/>
