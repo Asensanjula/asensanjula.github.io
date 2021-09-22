742a0bc308b1ba5b2b0fefd7d9de3fd5d8198222
Asen Sanjula <asensanjula@gmail.com>
2021-09-22 20:15:37 +0530

    feat: intro page base details

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -4,8 +4,21 @@ import './intro.scss'
 function Intro(props) {
     return (
         <div className="intro" id="intro">
-            <div className="left"></div>
-            <div className="right"></div>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={"portfolio/assets/man.png"} alt="profile"/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello There, I'm</h2>
+                    <h1>Asen Sanjula</h1>
+                    <h3>Front-End Developer | React JS</h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="portfolio/assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     );
 }
