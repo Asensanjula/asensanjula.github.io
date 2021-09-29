f929fa83ffbd6c6cd69224c479ddd29d31846193
Asen Sanjula <asensanjula@gmail.com>
2021-09-29 16:22:38 +0530

    feat: contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './contact.scss'
 
 function Contact(props) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className="contact" id="contact">
-
+            <div className="left">
+                <img src={process.env.PUBLIC_URL + "/assets/shake.svg"} alt="Shake-icon"/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     );
 }
