23d5803b7a08b62f190dbfb71fe361450d787dac
Asen Sanjula <asensanjula@gmail.com>
2021-10-12 11:20:21 +0530

    feat: skills page responsive design

--- src/App.js
+++ src/App.js
@@ -8,17 +8,11 @@ import './app.scss'
 import Menu from "./components/menu/Menu";
 import Particles from 'react-particles-js';
 import About from "./components/about/About";
-import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Experience from "./components/experience/Experience";
 import Skill from "./components/skill/Skill";
 
 function App() {
-
-    useEffect(() => {
-        AOS.init({ duration: 1000 });
-        console.log("Aos init > ", AOS)
-    }, []);
 
     const [openMenuBar, setOpenMenuBar] = useState(false);
     return (
--- src/components/skill/Skill.js
+++ src/components/skill/Skill.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./skill.scss"
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
 
 function Skill(props) {
     return (
@@ -9,36 +10,116 @@ function Skill(props) {
             </div>
             <div className="right">
                 <div className="wrapper">
+                    <OverlayTrigger
+                        key={"top-1"}
+                        placement={'top'}
+                        overlay={
+                            <Tooltip id={`tooltip-top`}>
+                                HTML
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img className="svgImg" src={process.env.PUBLIC_URL + "/assets/skills/html.svg"} alt=""/>
                     </div>
-                    <div className="skillItem">
-                        <img src={process.env.PUBLIC_URL + "/assets/skills/css.svg"} alt=""/>
-                    </div>
+                    </OverlayTrigger>
+                    <OverlayTrigger
+                        key={"top-2"}
+                        placement={'top'}
+                        overlay={
+                            <Tooltip id={`tooltip-top`}>
+                                CSS
+                            </Tooltip>
+                        }
+                    >
+                        <div className="skillItem">
+                            <img src={process.env.PUBLIC_URL + "/assets/skills/css.svg"} alt=""/>
+                        </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"top-3"}
+                        placement={'top'}
+                        overlay={
+                            <Tooltip id={`tooltip-top`}>
+                                JAVASCRIPT
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/js.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"top-4"}
+                        placement={'top'}
+                        overlay={
+                            <Tooltip id={`tooltip-top`}>
+                                REACT JS
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/react.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"bottom-1"}
+                        placement={'bottom'}
+                        overlay={
+                            <Tooltip id={`tooltip-bottom`}>
+                                SCSS
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/sass.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"bottom-2"}
+                        placement={'bottom'}
+                        overlay={
+                            <Tooltip id={`tooltip-bottom`}>
+                                REDUX
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/redux.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"bottom-3"}
+                        placement={'bottom'}
+                        overlay={
+                            <Tooltip id={`tooltip-bottom`}>
+                                BOOTSTRAP
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/boostrap.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
 
+                    <OverlayTrigger
+                        key={"bottom"}
+                        placement={'bottom'}
+                        overlay={
+                            <Tooltip id={`tooltip-bottom`}>
+                                REDUX SAGA
+                            </Tooltip>
+                        }
+                    >
                     <div className="skillItem">
                         <img src={process.env.PUBLIC_URL + "/assets/skills/saga.svg"} alt=""/>
                     </div>
+                    </OverlayTrigger>
                 </div>
             </div>
         </div>
